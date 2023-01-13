487e114650acdb9fbfa4713059eb19db8f651b42
gs20wow@gmail.com <gs20wow@gmail.com>
2023-01-13 19:56:53 +0900

    0113 오르막수 해결

--- Silver/BOJ11057오르막수.py
+++ Silver/BOJ11057오르막수.py
@@ -1,11 +1,24 @@
 #BOJ11057_오르막 수 
+#런타임 오류
+# import sys
+# input = sys.stdin.readline
+
+# N = int(input())
+# dp = list([1]*10 for _ in range(10))
+# for i in range(1, N):
+#     for j in range(1, 10): 
+#         dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+# print(sum(dp[N-1])%10007)
+
+
 import sys
 input = sys.stdin.readline
+dp = [1]*10
 
 N = int(input())
-dp = list([1]*10 for _ in range(10))
 for i in range(1, N):
-    for j in range(1, 10): 
-        dp[i][j] = dp[i-1][j] + dp[i][j-1]
-
-print(sum(dp[N-1])%10007)
+    for j in range(1, 10):
+        dp[j] += dp[j-1]
+        
+print(sum(dp)%10007)
