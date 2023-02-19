4769ba2f6932502e16daedf668412a6cd08ede9b
gs20wow@gmail.com <gs20wow@gmail.com>
2023-02-19 22:29:51 +0900

    0219 b2-boj20155 우리집밑편의점-단순구현

---
+++ Bronze/BOJ20155최대.py
@@ -0,0 +1,11 @@
+#BOJ20155_우리집밑편의점
+#https://www.acmicpc.net/problem/20155
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = []
+
+for i in range(0, max(arr) + 1):  
+    cnt.append(arr.count(i))  # 각 편의점의 개수 세기
+
+print(max(cnt))  # 편의점 개수 중 가장 큰 수 출력
