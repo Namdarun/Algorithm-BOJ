7ef9944675407cb2376e4c46327c59ff17dbccd1
gs20wow <gs20wow@gmail.com>
2023-02-28 08:03:51 +0900

    20230227 G5,S4 BOJ 도넛행성, 카드2 - stack, deque

--- Gold/BOJ27211도넛행성.py
+++ Gold/BOJ27211도넛행성.py
@@ -1,7 +1,7 @@
 #BOJ27211_도넛행성_G5
 #https://www.acmicpc.net/problem/27211
 
-# 1로 둘러쌓여져 있거나, 따로 붙어있는 경우를 어떻게 체크하면 좋을까?
+
 n, m = map(int, input().split())
 donut = [list(map(int, input().split())) for _ in range(n)]
 
@@ -11,8 +11,11 @@ def bfs(a, b):
     while stack:
         # x, y = stack.pop()
         x, y = stack.pop()
+        # 4방향 탐색 
         for di, dj in [[0,1], [1,0], [0,-1], [-1,0]]:
             ni, nj = x+di, y+dj
+            # 옮긴 지역이 n과 같을 땐 초기화
+            # 작아지면 그 뒤 노드에 안착
             if ni == n:
                 ni = 0
             elif ni < 0:
@@ -21,6 +24,7 @@ def bfs(a, b):
                 nj = 0
             elif nj < 0:
                 nj = m - 1
+            # 방문한 적이 없으면 1로 만들어주고 스택에 추가
             if not donut[ni][nj]:
                 donut[ni][nj] = 1
                 stack.append((ni, nj))
