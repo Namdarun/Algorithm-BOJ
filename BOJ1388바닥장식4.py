694f1891829b59bddd11c8c6d18dd321563a4525
Namdarun <gs20wow@gmail.com>
2023-03-21 22:57:25 +0900

    20230321 BOJ14501_퇴사_S3_DP

---
+++ BOJ1388바닥장식4.py
@@ -0,0 +1,2 @@
+#BOJ1388_바닥장식4_S4
+#https://www.acmicpc.net/problem/1388   
---
+++ DP(Dynamic Programming)/BOJ14501퇴사.py
@@ -0,0 +1,42 @@
+#BOJ14501_퇴사_S3
+#https://www.acmicpc.net/problem/14501
+
+#전형적인 DP문제 
+#탑다운, 바텀업으로 나눠서 풀어보자
+#2차원 배열을 봐야하므로 2차원배열로 받는다.
+
+#bottom-up
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+time = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0 for _ in range(n+1)]
+
+#최대값을 dp에 저장해나간다. 
+for i in range(n):
+    for j in range(i+time[i][0], n+1):
+        if dp[j] < dp[i] + time[i][1]:
+            dp[j] = dp[i] + time[i][1]
+
+#마지막 저장값이 j를 기준으로 한 최대수익
+print(dp[-1])
+
+#top-down
+#거꾸로부터 계산해나가는 방식
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = [list(map(int, input().split())) for _ in range(n)]
+dp = [0 for _ in range(n+1)]
+
+for i in range(n-1, -1, -1):
+    if i + li[i][0] > n:
+        dp[i] = dp[i+1]
+
+    else:
+        dp[i] = max(dp[i+1], li[i][1] + dp[i+ li[i][0]])
+print(dp[0])
