e3190e2a82df8eb46d07a056e5980302ffa9ead8
gs20wow <gs20wow@gmail.com>
2023-04-05 20:08:49 +0900

    20230405 BOJ19532_수학은비대면강의_B2 단순구현

---
+++ Bronze/BOJ19532수학은비대면강의.py
@@ -0,0 +1,10 @@
+#BOJ19532_수학은비대면강의_B2
+#https://www.acmicpc.net/problem/19532
+
+#완탐, 흔한 연립방정식 
+a, b, c, d, e, f = map(int, input().split())
+
+for i in range(-999, 1000):
+    for j in range(-999, 1000):
+        if (a*i) + (b*j) == c and (d*i) + (e*j) == f:
+            print(i, j)
