ef28461954e1f5ae2ebbc7a697d31ea7a979902c
gs20wow <gs20wow@gmail.com>
2023-04-21 08:36:34 +0900

    20230432 BOJ15655_N과M6_S3 순열

---
+++ N과 M 시리즈/BOJ15655N과M6.py
@@ -0,0 +1,18 @@
+#BOJ15655_N과M6_S3
+#https://www.acmicpc.net/problem/15655
+
+def nPm(start):
+	if len(s) == M:
+		print(*s)
+		return
+	# 중복없이 
+	for i in range(start, N):
+		if arr[i] not in s:
+			s.append(arr[i])
+			nPm(i+1)
+			s.pop()
+
+N, M = map(int, input().split())
+arr = sorted(map(int, input().split()))
+s = []
+nPm(0)
