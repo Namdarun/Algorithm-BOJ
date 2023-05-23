5f572ba6c51e75758729355e2335ec3b0ec514ad
Namdarun <gs20wow@gmail.com>
2023-05-24 00:14:28 +0900

    20230524 BOJ28074_모비스_B4 단순구현

--- Bronze/BOJ28074모비스.py
+++ Bronze/BOJ28074모비스.py
@@ -1,8 +1,22 @@
 #BOJ28074_모비스_B4
 #https://www.acmicpc.net/problem/28074
 
-arr = list(input())
-if 'M' and 'O' and 'B' and 'I' and 'S' in arr: 
+# def check(arr):
+#     word = 'MOBIS'
+#     for i in arr:
+#         if i not in word:
+#             return False
+#         arr = arr.replace(i, '', 1)
+#     return True
+
+# check_string = input()
+# checking = check(check_string)
+# print('YES' if checking else 'NO')
+
+word = input()
+arr = list(set(word))
+# print(arr)
+if arr.count('M')+arr.count('O')+arr.count('B')+arr.count('I')+arr.count('S') == 5:
     print('YES')
 else:
     print('NO')
