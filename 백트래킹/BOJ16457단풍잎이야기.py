02acc8551d611301db947e68f3bd21a8409e7e77
Namdarun <gs20wow@gmail.com>
2023-06-20 17:34:45 +0900

    20230620 BOJ16457_단풍잎이야기_S1 단순구현

--- 백트래킹/BOJ16457단풍잎이야기.py
+++ 백트래킹/BOJ16457단풍잎이야기.py
@@ -1,3 +1,29 @@
 #BOJ16457_단풍잎이야기_S1
 #https://www.acmicpc.net/problem/16457
 
+n = int(input())
+s, g, p, d = list(map(int, input().split()))
+grade = input()
+sum = 0
+prev = 0
+
+#n만큼 돌면서 전달, 이번달 과금액에 sum
+# prev는 최대과금액으로 초기화
+for i in range(n):
+    if grade[i] == 'B':
+        sum += s - 1 - prev
+        prev = s - 1 - prev
+    elif grade[i] == 'S':
+        sum += g - 1 - prev
+        prev = g - 1 - prev
+    elif grade[i] == 'G':
+        sum += p - 1 - prev
+        prev = p - 1 - prev
+    elif grade[i] == 'P':
+        sum += d - 1 - prev
+        prev = d - 1 - prev
+    elif grade[i] == 'D':
+        sum += d
+        prev = d
+        
+print(sum)
