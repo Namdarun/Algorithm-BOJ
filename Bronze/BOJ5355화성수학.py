ecc8a4e019bc01827654bc54d9a52f31bb45cfe1
Namdarun <gs20wow@gmail.com>
2023-07-08 05:40:58 +0900

    20230708 BOJ5355_화성수학_B2 단순구현

---
+++ Bronze/BOJ5355화성수학.py
@@ -0,0 +1,18 @@
+#BOJ5355_화성수학_B2
+#https://www.acmicpc.net/problem/5355
+
+n = int(input())
+
+for i in range(n):
+    arr = list(input().split())
+    #수식
+    arr_num = float(arr.pop(0))
+    for i in range(len(arr)):
+        if arr[i] == '@':
+            arr_num *= 3
+        elif arr[i] == '%':
+            arr_num += 5
+        elif arr[i] == '#':
+            arr_num -= 7
+    
+    print("{:.2f}".format(arr_num))
