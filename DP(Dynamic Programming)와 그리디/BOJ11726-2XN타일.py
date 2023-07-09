756e69838ccf07cd8b6dbec578c6ffc2dbbc07ff
Namdarun <gs20wow@gmail.com>
2023-07-09 22:30:40 +0900

    20230709 BOJ11726_2XN 타일링 재풀이

--- DP(Dynamic Programming)와 그리디/BOJ11726-2XN타일.py
+++ DP(Dynamic Programming)와 그리디/BOJ11726-2XN타일.py
@@ -37,3 +37,18 @@ for i in range(3, 1001):
 	memo[i] = memo[i-1] + memo[i-2]
 	
 print(memo[n]%10007)
+
+
+
+
+#세번째 풀이
+n = int(input())
+#0,1,2 
+dp = [0, 1, 2]
+if n > 2:
+	# n+1 주의
+	for i in range(3, n+1):
+		dp.append(i)
+		dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n] % 10007)
