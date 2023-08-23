13bc45d8467b237d3b7dfa6df014ed3059d8bf93
Namdarun <gs20wow@gmail.com>
2023-08-23 20:57:51 +0900

    20230823 BOJ2468_안전영역_S1 DFS 수정

--- 완전탐색(그래프탐색)/BOJ2468안전영역.py
+++ 완전탐색(그래프탐색)/BOJ2468안전영역.py
@@ -59,3 +59,43 @@ for i in range(cnt):
 print(result)
 
 #DFS 추가해야함
+import sys
+# 재귀 깊이 설정
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+#4방향
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+def dfs(x, y):
+    visited[x][y] = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if (0 <= nx < n) and (0 <= ny < n) and visited[nx][ny]:
+            visited[nx][ny] = 1
+            dfs(nx, ny)
+
+    return 1
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+result = 0
+for h in range(101):
+    cnt = 0
+    visited = [[0]*n for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] > h:
+                visited[i][j] = arr[i][j]
+    
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                cnt += dfs(i, j)
+
+    result = max(result, cnt)
+print(result)
