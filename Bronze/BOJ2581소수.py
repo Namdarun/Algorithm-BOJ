e96030076d7797aa85c4526acafece6768980f0e
Namdarun <gs20wow@gmail.com>
2023-09-07 23:16:43 +0900

    20230907 BOJ2581_소수_B2 구현

---
+++ Bronze/BOJ2581소수.py
@@ -0,0 +1,19 @@
+#BOJ2581_소수_B2
+#https://www.acmicpc.net/problem/2581
+
+m = int(input())
+n = int(input())
+
+arr = []
+for i in range(m, n+1):
+    for j in range(2, i+1):
+        if j == i:
+            arr.append(i)
+        if i % j == 0:
+            break
+
+if not arr:
+    print(-1)
+else:
+    print(sum(arr))
+    print(arr[0])
